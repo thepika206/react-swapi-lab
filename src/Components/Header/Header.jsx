d424700340d71a0eca23211f50e621c593740023
pcheung <pika206@gmail.com>
2022-11-22 14:35:16 -0600

    remove red test style from title

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ import './Header.css';
 const Header = () => {
   return (  
       <header className="App-header">
-        <h1 id="title" >STAR WARS STARSHIPS</h1>
+        <h1 id="title" >STAR WARS FAN PAGE</h1>
         <nav>
           <NavLink to="/starships" className='nav-item'>Starships</NavLink>
           <NavLink to="/people" className='nav-item'>People</NavLink>
