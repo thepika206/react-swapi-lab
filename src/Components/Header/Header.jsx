2ab128bb8989e1c638c392eca164eb87802d2d45
pcheung <pika206@gmail.com>
2022-11-22 14:30:32 -0600

    style nav links so that active link is underlined

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,25 +1,17 @@
-import { Link } from "react-router-dom"
-import { NavLink } from "react-router-dom"
-import NavList from "../NavList"
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import './Header.css';
+// import NavList from "../NavList"
 
 const Header = () => {
   return (  
-    <>
-    <div className="App-header">
-      <h1>STAR WARS STARSHIPS</h1>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/starships">Starships</NavLink>
-          </li>
-          <li>
-            <NavLink to="/people">People</NavLink>
-          </li>
-        </ul>
-        
-      </nav>
-    </div>
-    </>
+      <header className="App-header">
+        <h1 id="title" >STAR WARS STARSHIPS</h1>
+        <nav>
+          <NavLink to="/starships" className='nav-item'>Starships</NavLink>
+          <NavLink to="/people" className='nav-item'>People</NavLink>
+        </nav>
+      </header>
   )
 }
 
