58f3370a8fc57152ec8ff80035e157c6c96bc0da
pcheung <pika206@gmail.com>
2022-11-25 16:51:17 -0600

    imp film list on person details

--- src/pages/PersonDetails/PersonDetails.jsx
+++ src/pages/PersonDetails/PersonDetails.jsx
@@ -1,9 +1,22 @@
 import { useLocation } from "react-router-dom"
 import { Link } from "react-router-dom"
+import FilmList from "../../Components/FilmsList/FilmsList"
+import { useEffect, useState } from "react"
+import { getRelatedObjects } from "../../services/api-calls"
 
 
 const PersonDetails = () => {
   const location = useLocation()
+  const [films, setFilms] = useState([])
+  const filmUrls = location.state.person.films
+
+  useEffect(() =>{
+    const fetchFilmData = async() => {
+      const filmData = await getRelatedObjects(filmUrls)
+      setFilms(filmData)
+    }
+    fetchFilmData()
+  },[filmUrls])
 
   return (  
     <div className="container-centered">
@@ -14,6 +27,9 @@ const PersonDetails = () => {
         <h4>HEIGHT: {location.state.person.height}</h4>
         <h4>MASS: {location.state.person.mass}</h4>
         {/* <h4>HOMEWORLD: {location.state.person.homeworld}</h4> */}
+        <FilmList
+          films = {films}
+        />
         <Link
           to="/people"
           >
