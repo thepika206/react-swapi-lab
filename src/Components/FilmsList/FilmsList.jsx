92358fe590c31da7047d95145f305a61fb7d78ea
pcheung <pika206@gmail.com>
2022-11-26 08:06:52 -0600

    format filmlist

--- src/Components/FilmsList/FilmsList.jsx
+++ src/Components/FilmsList/FilmsList.jsx
@@ -3,19 +3,15 @@ const FilmList = (props) => {
     <>
       <h3>Film List</h3>
       {
-        
         (props.films.length===0) 
-        ? <p>loading films data</p> 
-        : 
-        props.films.map(film => 
-          <p
-            key = {film.title}
-          >{film.title}</p>)
-        
-        
+          ? <p>loading films data</p> 
+          :props.films.map(film => 
+            <p
+              key = {film.title}
+            >{film.title}</p>)
       }
     </>
   )
 }
- 
+
 export default FilmList;
