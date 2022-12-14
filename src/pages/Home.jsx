e5b3611c1fb57aa239f936388b5dba64fa963cb5
pcheung <pika206@gmail.com>
2022-12-14 10:32:41 -0600

    home page text update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,11 @@
 const Home = () => {
   return (  
     <div className="container-centered">
-      <h2>Discover Starships from the Star Wars film franchise</h2>
+      <h2>A "wookiepedia" site about ships and people from Star Wars</h2>
       <img src="/Rebel_Alliance_logo.svg" alt="rebel alliance symbol" />
-      <h3>Credits</h3>
-      <p>This site receives data from SWAPI https://swapi.dev/ an API dedicated to providing information about all things Starwars.</p>
+      <h3>About</h3>
+      <p>I build this site in React and have been steadily consuming more and more functionality of the free public SWAPI API</p>
+      <p>SWAPI https://swapi.py4e.com/api an API dedicated to providing information about all things Starwars.</p>
     </div>
   )
 }
