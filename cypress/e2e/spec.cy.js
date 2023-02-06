b4bfa3f8743dfe44cf93b4118eba51d7f2a16800
Patrick Cheung <patrick@inquired.org>
2023-02-06 09:15:42 -0600

    reorg paging tests, add tests for starship details

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,49 +1,28 @@
 
-
-describe('My First Test', () => {
-  it('finds the content "type"', () => {
-    cy.visit('https://example.cypress.io')
-    
-    cy.contains('type').click()
-    
-    // looking for a new URL that includes 
-    //'/commands/actions'
-    cy.url().should('include', '/commands/actions')
-
-    // Get an input with the class of "action-email", type into it
-    cy.get('.action-email').type('fake@email.com')
-
-    // Verify that the value has been updated
-    cy.get('.action-email').should('have.value', 'fake@email.com')
-  })
-})
-
-// future section of tests 
-
-// Production tests
-// describe('Production Test', () => {
-//   it('It visits the deployed app home page', () => {
-//     cy.visit('https://pcheung-sw-api.netlify.app/')
-    
-//   })
-// })
-
-describe('Local Navbar Test', () => {
-  it('It visits the deployed app home page', () => {
+describe('Local app test', () => {
+  it('Verifies navigation to starships, people and home', () => {
     cy.visit('http://localhost:3000/')
-
-//select the home-nav-item
     cy.get('[id=home-nav-item]').should('have.class', 'active')
-
-//navigate to starships and advance pages
     cy.get('[id=starships-nav-item]').click().should('have.class', 'active')
+    cy.get('[id=people-nav-item]').click().should('have.class', 'active')
+    cy.get('[id=home-nav-item]').click().should('have.class', 'active')
+  })
+
+  it('Verifies changing pages on Starship list and should return to page 1', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[id=starships-nav-item]').click()
     cy.get('button[id=next-btn]').click()
     cy.get('p[id=page-counter-p]').should('have.text', 'Page: 2 of 4')
     cy.get('button[id=prev-btn]').click()
     cy.get('p[id=page-counter-p]').should('have.text', 'Page: 1 of 4')
+  })
 
-//select the people-nav-item
-    // cy.get('[id=people-nav-item]').click().should('have.class', 'active')
-
+  it('Verifies Starship details: opens Death Star details then returns to Starships list', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[id=starships-nav-item]').click()
+    cy.get('.card').contains('Death Star').click()
+    cy.get('h2').contains('Death Star')
+    cy.get('button').contains('Back to All Starships').click()
+    cy.get('p[id=page-counter-p]').should('have.text', 'Page: 1 of 4')
   })
 })
