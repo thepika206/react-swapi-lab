9857b594ac948c690d4d826f57831b9fd3ebc7de
Patrick Cheung <pika206@gmail.com>
2023-10-16 12:42:33 -0500

    formatting

--- src/Components/PilotList/PilotList.jsx
+++ src/Components/PilotList/PilotList.jsx
@@ -1,1 +1,26 @@
+import { Link } from "react-router-dom";
+const PilotList = (props) => {
+  return (
+    <>
+      <h3>Pilot List</h3>
+      {
 
+        (props.pilots.length === 0)
+          ? <p>no pilots</p>
+          :
+          props.pilots.map(pilot =>
+            <Link
+              to={`/people/${pilot.url.slice(34)}`}
+              key={pilot.name}
+            >
+              <p>{pilot.name}</p>
+            </Link>
+          )
+
+
+      }
+    </>
+  )
+}
+
+export default PilotList;
